49800182e20fee95d3f8641355061c7f8090fdb0
Raí Siqueira <rai.siqueira@justa.com.vc>
2019-04-11 11:29:59 -0300

    Init
    Signed-off-by: Raí Siqueira <rai.siqueira@justa.com.vc>

---
+++ src/Store.ts
@@ -0,0 +1,35 @@
+import { Observable, BehaviorSubject } from "rxjs";
+
+export abstract class Store<S> {
+    /**
+     * Observable from our state
+     */
+    public state$: Observable<S>;
+
+    /**
+     * @internal
+     */
+    private _state$: BehaviorSubject<S>;
+
+    protected constructor(initialValue: S) {
+        // set the store value with initialValue
+        this._state$ = new BehaviorSubject(initialValue);
+        // set our state as Observable
+        this.state$ = this._state$.asObservable();
+    }
+
+    /**
+     * get current state
+     */
+    get state(): S {
+        return this._state$.getValue();
+    }
+
+    /**
+     * setState - update our state with a new value
+     * @param nextState
+     */
+    public setState(nextState: S): void {
+        this._state$.next(nextState)
+    }
+}
