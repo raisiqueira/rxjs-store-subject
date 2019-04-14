7018438623532bc1ea32076950b4cf8de82e5e8c
Raí Siqueira <rai93siqueira@gmail.com>
2019-04-14 19:06:41 -0300

    Prettier for Store file
    Signed-off-by: Raí Siqueira <rai93siqueira@gmail.com>

--- src/Store.ts
+++ src/Store.ts
@@ -1,36 +1,36 @@
 import { Observable, BehaviorSubject } from "rxjs";
 
 export abstract class Store<S> {
-    /**
-     * Observable from our state
-     * @template S - State
-     */
-    public state$: Observable<S>;
+  /**
+   * Observable from our state
+   * @template S - State
+   */
+  public state$: Observable<S>;
 
-    /**
-     * @internal
-     */
-    private _state$: BehaviorSubject<S>;
+  /**
+   * @internal
+   */
+  private _state$: BehaviorSubject<S>;
 
-    protected constructor(initialValue: S) {
-        // set the store value with initialValue
-        this._state$ = new BehaviorSubject(initialValue);
-        // set our state as Observable
-        this.state$ = this._state$.asObservable();
-    }
+  protected constructor(initialValue: S) {
+    // set the store value with initialValue
+    this._state$ = new BehaviorSubject(initialValue);
+    // set our state as Observable
+    this.state$ = this._state$.asObservable();
+  }
 
-    /**
-     * get current state
-     */
-    get state(): S {
-        return this._state$.getValue();
-    }
+  /**
+   * get current state
+   */
+  get state(): S {
+    return this._state$.getValue();
+  }
 
-    /**
-     * setState - update our state with a new value
-     * @param nextState
-     */
-    public setState(nextState: S): void {
-        this._state$.next(nextState)
-    }
+  /**
+   * setState - update our state with a new value
+   * @param nextState
+   */
+  public setState(nextState: S): void {
+    this._state$.next(nextState);
+  }
 }
